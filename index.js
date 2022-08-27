f5ec2a34450e8f97419dd6ecd7e9eb0a21b08797
Emanuel <emanuel.vital.falcao@gmail.com>
2022-08-27 11:57:26 -0300

    Alteracao do index.js

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ receba dois valores, que devem ser salvos em variáveis;
 o usuário deve colocar qual operador ele vai utilizar por meio dos símbolos aritméticos; 
 com os dois valores e o operador definido, o programa deve fazer a operação e retornar o resultado; 
 se houver divisão, você deve retornar o resultado e a sobra, caso haja alguma. 
+
+Escolha algum código executável em JavaScript feito em atividades passadas e o coloque no Node.js. 
+Para isso, você deve ter o Node.js instalado. Após fazer essa migração, coloque seu código no arquivo "index.js", 
+teste e tire um print.
+
+Trabalhe esse código em seu IDE, suba ele para sua conta no GitHub e compartilhe o link desse projeto no campo ao 
+lado para que outros desenvolvedores possam analisá-lo.
 */
 
 console.log("Desenvolvimento 07 - CalculadoraNode");
@@ -23,9 +30,7 @@ if (operacao == "+") {
 			console.log("A multiplicacao eh: " + multiplicacao)
 }	else if (operacao == "/") {
 		var divisao = numero_01/numero_02;
-		var modulo= numero_01 % numero_02;
-  // var final = modulo - divisao;
-			console.log("A divisao eh: " + divisao + " e o resto " + modulo)
+		console.log("A divisao eh: " + divisao)
 } else {
 	console.log("Operacao Invalida.")
 }
